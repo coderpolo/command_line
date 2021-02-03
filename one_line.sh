2f954e4e344a0968ba58bbfad6ba467ff7d6f524
wukaisheng <wukaisheng@xiaomi.com>
2021-02-03 10:28:09 +0800

    one_line

--- one_line.sh
+++ one_line.sh
@@ -27,6 +27,9 @@ git remote -v | awk '{print $2}' | sed -n 1p | cb
 #利用fzf的模糊搜索，交互式切换分支
 git checkout $(gb | fzf)
 
+#统计git提交代码行数
+git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --author="$name" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -; done
+
 #用sql直接查询文本文件。http://harelba.github.io/q/
 q -d $',' "select c1,max(c3) from ./txt_file group by c1"
 
