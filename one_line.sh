b0636e2db2c8e362ca6d42c1874ff0cdafcf9294
wukaisheng <564336652@qq.com>
2022-05-09 19:18:36 +0800

    log

--- one_line.sh
+++ one_line.sh
@@ -142,3 +142,9 @@ sed -i '1d,3d' fileName
 
 #对于比较重要的文件 删除最好用重定向，以原地删除为例
 sed '1d' fileName >tmpfile && mv tmpfile fileName
+
+#使用命令添加定时任务-可将这条命令以脚本的方式拷贝到各个主机。然后用xargs触发各个主机使用此脚本
+(echo "*/1 0-10 * * * (rm -rf /tmp)"; crontab -l)|crontab
+
+#批量清空各个机器的定时任务
+cat hostFile |  xargs -I machine ssh -T -o StrictHostKeyChecking=no machine "hostname && crontab -r "
