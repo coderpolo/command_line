cc6cd7a3f73c22e836411dc476d7bd8f48f9f718
wukaisheng <564336652@qq.com>
2022-07-05 10:01:49 +0800

    sed

--- one_line.sh
+++ one_line.sh
@@ -132,25 +132,6 @@ ls -lShr
 #时间戳转日期。登服务器的时候用的比较多，不用再粘贴到网页了
 date -d@1648387801
 
-#打印指定行
-sed -n '1p' fileName
-#打印不连续行
-sed -n '1p;3p' fileName
-#打印连续行
-sed -n '1,3p' fileName
-
-#原地删除指定行
-sed -i '1d' fileName
-#原地删除不连续行
-sed -i '1d;3d' fileName
-#删除连续行
-sed -i '1d,3d' fileName
-
-#对于比较重要的文件 删除最好用重定向，以原地删除为例
-sed '1d' fileName >tmpfile && mv tmpfile fileName
-
-#sed替换指定行内的字符 -- 可结合xargs
-sed -i '262,265s/x/y/' filename
 
 #使用命令添加定时任务-可将这条命令以脚本的方式拷贝到各个主机。然后用xargs触发各个主机使用此脚本
 (echo "*/1 0-10 * * * (rm -rf /tmp)"; crontab -l)|crontab
