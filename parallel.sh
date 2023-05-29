fe482e50192bc3da97d60d82f2344553c432fe27
wukaisheng <564336652@qq.com>
2023-05-29 13:59:54 +0800

    对文件中的文本并发使用shell函数处理

--- parallel.sh
+++ parallel.sh
@@ -6,6 +6,6 @@ process_bucket() {
 # 导出函数，以便xargs调用
 export -f process_bucket
 
-# 逐行读取S3桶名，并使用xargs和parallel控制并发进程数
+# 逐行读取S3桶名，并使用xargs控制并发进程数
 cat "$bucket_list_file" | xargs -P "$concurrency" -I{} bash -c 'process_bucket "$@"' _ {}
 
