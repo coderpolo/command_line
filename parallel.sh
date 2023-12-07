7d21394889352919098259e0352f48ed6b6867fe
wukaisheng <564336652@qq.com>
2023-12-07 11:48:47 +0800

    1

--- parallel.sh
+++ parallel.sh
@@ -9,3 +9,5 @@ export -f process_bucket
 # 逐行读取S3桶名，并使用xargs控制并发进程数
 cat "$bucket_list_file" | xargs -P "$concurrency" -I{} bash -c 'process_bucket "$@"' _ {}
 
+# 从远端拷贝文件到中心机器
+pslurp -h ../hostfile "remot_file_name" local_path
